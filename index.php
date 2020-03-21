eea95e43421715a62c4d8ed78e67fe03dcb33e95
conradoRC <conrado27cym@gmail.com>
2020-03-21 10:10:36 -0600

    Sesiones

--- index.php
+++ index.php
@@ -1,20 +1,34 @@
 <?php 
-$loginSuccessful=false;
-
-if(isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])){
-    $userName=$_SERVER['PHP_AUTH_USER']; 
-    $userPassword=$_SERVER['PHP_AUTH_PW'];
-    if($userName=='Conrad' && $userPassword=='holi'){
-        $loginSuccessful=true;
+    session_start(['name'=> 'CONRAD']); //Cada vez que usemos sesiones
+    //session_destroy();  //Eliminar sessiones
+    unset($_SESSION['nameSession']); //Eliminar una session
+    if(session_status()==PHP_SESSION_DISABLE){
+        
     }
-}
+    if(isset($_SESSION['nameSession'])){
+        echo 'Bienvenido : '.$_SESSION['nameSession'];
+        $_SESSION['nameSession']='Cliente'; //Modificar el valor de la session
 
-if(!$loginSuccessful){
-    header('WWW-Authenticate:Basic');       
-    header('HTTP/1.0 401 Unauthorizated');
-    echo "Error, acceso denegado";
-}
-else{
-    echo "Bienvenido";
-}
+    }
+    else{
+        echo "Sesión no iniciada";
+    }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action='login_session.php' method='post'>
+        Usuario   : <input type="text" name='userName'></br>
+        Contraseña: <input type="text" name='userPassword'></br>
+       <input type="submit" name='send' text='Enviar'>
+    </form>
+
+</body>
+</html>
